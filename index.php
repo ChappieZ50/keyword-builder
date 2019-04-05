5adb3b2fcaff50feb9532023c0d7d7f9905ba660
ChappieZ50 <ugurtosun50@gmail.com>
2019-04-05 22:37:04 +0300

    Lorem

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . "/vendor/autoload.php";
-$text = 'Merhaba benim adım uğur. 18 Yaşımdayım, yakında 19 olacağım. Bu uygulamayı can sıkıntısından yapıyorum.asdasd asdasdasdas asdasd asd asd';
+$text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, consectetur ea fuga harum omnis quam? Culpa doloremque impedit ipsam laudantium nisi odio officia perspiciatis, provident quia reprehenderit tempora tempore veritatis.';
 
 $keyword_builder = new App\Lib\KeywordBuilder($text,1);
 $keywords = $keyword_builder->searchInDomain('https://github.com');
@@ -9,3 +9,4 @@ $href_adder->hrefAdder(['ucwords']);*/
 
 $add_in_text = new \App\Lib\KeywordAddInText($keywords,$text);
 echo $add_in_text->keywordAdder();
+?>
